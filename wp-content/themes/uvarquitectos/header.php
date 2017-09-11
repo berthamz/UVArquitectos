6ad583170c5cb3877911a92c5417a8c275cbd4eb
berthamz <berthamz@gmail.com>
2017-09-11 13:45:49 -0700

    Entrega prefinal

--- wp-content/themes/uvarquitectos/header.php
+++ wp-content/themes/uvarquitectos/header.php
@@ -3,6 +3,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+<meta name="description" content="<?php bloginfo('description'); ?>" />
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
 </head>
